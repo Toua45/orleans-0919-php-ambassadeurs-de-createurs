ccfb4f26bf8e65581c003d84a22b066490813f22
Toua <toua.va45@gmail.com>
2020-07-30 14:59:59 +0200

    fix on code about show favorite on profil page (add pagination)

--- src/Controller/FavoriteController.php
+++ src/Controller/FavoriteController.php
@@ -11,20 +11,26 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 
 class FavoriteController extends AbstractController
 {
+    const NB_MAX_FAVORITES = 12;
+
     /**
-     * @Route("/favorite", name="app_favorite")
+     * @Route("/favorite/{page}", name="app_favorite", requirements={"page" = "\d+"}, defaults={"page" = 1})
      * @param FavoriteRepository $favoriteRepository
      * @return Response
      * @IsGranted("ROLE_USER")
      */
 
-    public function show(FavoriteRepository $favoriteRepository): Response
+    public function show(FavoriteRepository $favoriteRepository, int $page): Response
     {
         $user = $this->getUser();
+        $nbFavorites = count($favoriteRepository->findFavoriteByUser($user));
+        $favorites = $favoriteRepository->findFavoriteByUser($user, $page);
+//        dd($favorites);
 
-        $favorites = $favoriteRepository->findFavoriteByUser($user);
         return $this->render('favorite/index.html.twig', [
-            'favorites' => $favorites
+            'favorites' => $favorites,
+            'page' => $page,
+            'nbPages' => ceil($nbFavorites / self::NB_MAX_FAVORITES),
         ]);
     }
 }
--- src/Repository/FavoriteRepository.php
+++ src/Repository/FavoriteRepository.php
@@ -2,6 +2,7 @@
 
 namespace App\Repository;
 
+use App\Controller\FavoriteController;
 use App\Entity\Favorite;
 use App\Entity\User;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
@@ -20,15 +21,20 @@ class FavoriteRepository extends ServiceEntityRepository
         parent::__construct($registry, Favorite::class);
     }
 
-    public function findFavoriteByUser($userId)
+    public function findFavoriteByUser($userId, ?int $page = null)
     {
         $qb = $this->createQueryBuilder('f')
             ->join('f.user', 'u')
             ->join('f.userFavorite', 'uf')
             ->where("f.user = :userId")
-            ->setParameter('userId', $userId)
-            ->getQuery();
+            ->orderBy('uf.firstname', 'ASC')
+            ->setParameter('userId', $userId);
 
-        return $qb->execute();
+        if ($page !== null) {
+                $firstResult = ($page - 1) * FavoriteController::NB_MAX_FAVORITES;
+                $qb->setFirstResult($firstResult)->setMaxResults(FavoriteController::NB_MAX_FAVORITES);
+        }
+
+        return $qb->getQuery()->execute();
     }
 }
